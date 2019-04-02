353544560f15ee9cd6035ed15e4d95b852784e31
Slowpuncher24 <ga74vuf@mytum.de>
2019-04-02 17:12:04 +0200

    Vastly improving warmup-stage and added lines for GPU/CUDNN-support

---
+++ module/cfd_python/linear_convection/generate_data.py
@@ -0,0 +1,87 @@
+import numpy as np
+from .. import common_methods as com
+# import matplotlib.pyplot as plt
+# import matplotlib.cm as cm
+# from mpl_toolkits.mplot3d import Axes3D
+
+
+def generate(options):
+    """
+    Generating data / function-values on a regular grid of space-time, adding noise and taking a batch of
+    down-sampled regular sub-grids of this grid. This batch will contain the samples to train our network with.
+
+    :param options: The dictionary of user-specified options (cf. main.py). Contains e.g. the grid-dimensions, the noise
+    :return: A batch (as a list) of samples (as dictionaries), that in turn consist of (noisy) function values on
+             down-sampled sub-grids for all dt-layers.
+    """
+
+    # Variable declarations
+    nx = options['mesh_size'][0]
+    ny = options['mesh_size'][1]
+    nt = options['layers']
+    dt = options['dt']
+    noise_level = options['noise_level']
+    downsample_by = options['downsample_by']
+    batch_size = options['batch_size']
+
+    a = 0
+    b = -10
+
+    dx = 2 * np.pi / (nx - 1)
+    dy = 2 * np.pi / (ny - 1)
+
+    ## Needed for plotting:
+    # x = np.linspace(0, 2*np.pi, num = nx)
+    # y = np.linspace(0, 2*np.pi, num = ny)
+    # X, Y = np.meshgrid(x, y)
+
+    ############ Change the following lines to implement your own data ############
+
+    ## Assign initial function:
+    u = com.initgen(options['mesh_size'], freq=4, boundary='Periodic')
+
+    ## Plotting the initial function:
+    # fig = plt.figure(figsize=(11,7), dpi=100)
+    # ax = fig.gca(projection='3d')
+    # surf = ax.plot_surface(X, Y, u[:], cmap=cm.viridis)
+    #
+    # plt.show()
+
+    sample = {}
+    sample['u0'] = u
+
+    for n in range(nt - 1):
+        un = u.copy()
+
+        u[1:-1, 1:-1] = un[1:-1, 1:-1] - dt * (
+                    a * (un[1:-1, 1:-1] - un[1:-1, :-2]) / dx + b * (un[1:-1, 1:-1] - un[:-2, 1:-1]) / dy)
+
+        u = com.pad_input_2(u[1:-1, 1:-1], 1)
+        # u[0,:] = 1
+        # u[-1,:] = 1
+        # u[:,0] = 1
+        # u[:,-1] = 1
+
+        sample['u' + str(n + 1)] = u
+
+    ## sample should at this point be a dictionary with entries 'u0', ..., 'uL', where L = nt                   ##
+    ## For a given j, sample['uj'] is a matrix of size nx x ny containing the function values at time-step dt*j ##
+    ###############################################################################
+
+    batch = []
+
+    ## Plotting the function values from the last layer:
+    # fig2 = plt.figure()
+    # ax2 = fig2.gca(projection='3d')
+    # surf2 = ax2.plot_surface(X, Y, u, cmap=cm.viridis)
+    #
+    # plt.show()
+
+    for i in range(batch_size):
+        sample_tmp = sample.copy()
+        com.downsample(sample_tmp, downsample_by)
+        com.addNoise(sample_tmp, noise_level, nt)
+
+        batch.append(sample_tmp)
+
+    return batch
---
+++ module/main.py
@@ -0,0 +1,36 @@
+import inferring_the_pde
+import tensorflow as tf
+import time
+
+options = {'mesh_size': [250,250],      # How large is the (regular) 2D-grid of function values for a fixed t.
+                                        # Keep mesh_size[0] = mesh_size[1]
+           'layers': 9,                 # Layers of the NN. Also counting the initial layer!
+           'dt': 0.015,                 # Time discretization. We step dt*(layers - 1) forward in time.
+           'batch_size': 24,             # We take a batch of sub-grids in space
+           'noise_level': 0.0,          # Can add some noise to the data (not taken 1 to 1, gets multiplied by stddev)
+           'downsample_by': 5,          # Size of sub-grids (in space) * downsample_by = mesh_size
+           'filter_size' : 7,           # Size of filters to approximate derivatives via FD
+           'iterations' : 5,            # How often to repeat optimization in the warmup-step
+           'max_order' : 2,             # Max-order of our hidden PDE. Note: max_order < filter_size!
+           'boundary_cond' : 'PERIODIC' # Set to 'PERIODIC' if data has periodic bdry condition to use periodic padding
+           }
+
+t0 = time.time()
+
+a = inferring_the_pde.OptimizerClass(options)
+
+# Repeat the warmup to hopefully find the global optimum while keeping
+# the moment-matrices fixed and thus the number of parameters low
+coefs, _, _ = a.optimize_weights(stage='WARMUP', iterations=options['iterations'])
+
+print('----------------------------------------------')  # End of warmup
+
+# Optimizing for t>0 while partially freeing up the moment-matrices as well
+with tf.variable_scope('normal_%d' % 1):
+    coefs, moment_matrices, _ = a.optimize_weights(stage='NORMAL', coefs=coefs, layer=1)
+for l in range(2, options['layers']):
+    with tf.variable_scope('normal_%d' % l):
+        coefs, moment_matrices, _ = a.optimize_weights(stage='NORMAL', coefs=coefs, layer=l,
+                                                       moment_matrices=moment_matrices)
+
+print('Program ran for %d seconds' % (time.time() - t0))
